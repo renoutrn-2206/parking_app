204cebc9d5db9b20edf15b8423268ba6131fcb77
Renat Nabiev <renout.rn@gmail.com>
2026-02-13 00:07:30 +0500

    Fix factories import in tests

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -1,7 +1,7 @@
 from factories import ClientFactory, ParkingFactory
 
 from app import db
-from app.models import Client, Parking
+from app.factories import Client, Parking
 
 
 def test_create_client_with_factory(app):
